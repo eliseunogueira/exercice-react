2f56c2297b8dc66dc2649c93c94292d7ad1b6b39
eliseunogueira <eliseu.nogueira@outlook.com>
2020-01-21 20:40:57 -0300

    adicionando deploy

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
     const { monsters, searchField } = this.state;
     const filteredMosters = monsters.filter(moster =>
@@ -22,9 +25,10 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList mosters={filteredMosters} />
       </div>
