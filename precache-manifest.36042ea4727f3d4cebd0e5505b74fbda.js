93690ed7584728538b9e3c181fd5bea3f4b06296
eliseunogueira <eliseu.nogueira@outlook.com>
2020-01-21 20:44:03 -0300

    Updates

---
+++ precache-manifest.36042ea4727f3d4cebd0e5505b74fbda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067f6efec11c064ab8ded501bde08ba0",
+    "url": "/exercice-react/index.html"
+  },
+  {
+    "revision": "17d08dea8517a8f15a13",
+    "url": "/exercice-react/static/css/main.7226da5e.chunk.css"
+  },
+  {
+    "revision": "0193fd501bbadc1438a6",
+    "url": "/exercice-react/static/js/2.c9114c39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/exercice-react/static/js/2.c9114c39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17d08dea8517a8f15a13",
+    "url": "/exercice-react/static/js/main.fe56a291.chunk.js"
+  },
+  {
+    "revision": "ff657259f07feac34954",
+    "url": "/exercice-react/static/js/runtime-main.aceef71a.js"
+  }
+]);
